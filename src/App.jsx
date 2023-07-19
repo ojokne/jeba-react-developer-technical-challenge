23b21787f48ae7b619ae509b0392fc8ae97125c3
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2023-07-19 21:16:37 +0300

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Home from "./components/Home";
+import Get from "./components/Get";
+import Post from "./components/Post";
+import Put from "./components/Put";
+import Delete from "./components/Delete";
 
 function App() {
   return (
-    <div>
-      <h1>Vite + React</h1>
+    <div className="d-flex justify-content-center align-items-center p-3">
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="get" element={<Get />} />
+          <Route path="post" element={<Post />} />
+          <Route path="put" element={<Put />} />
+          <Route path="delete" element={<Delete />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
